5cf407a28a6975fe451fe78884e1d4ed9950ba4b
Unknown <ruben.de.neckere@student.ehb.be>
2017-11-15 19:40:56 +0100

    dataAcces
    dataAcces voor surveyQ/A

---
+++ src/main/java/dataAccess/SurveyAnswerAcces.java
@@ -0,0 +1,35 @@
+package dataAccess;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import models.SurveyAnswer;;
+
+public class SurveyAnswerAcces extends RestRequest {
+	
+	public static SurveyAnswer get(Integer surveyQuestionID) throws IOException, URISyntaxException {
+		String JSONAdr = getAllOrOne(new URI(Constants.ADDRESS_SOURCE + surveyQuestionID));
+		SurveyAnswer surveyQuestion= mapper.readValue(JSONAdr, SurveyAnswer.class);
+		return surveyQuestion;
+	}
+
+
+public static HashMap<Integer, SurveyAnswer> getAll() throws IOException, URISyntaxException {
+	String JSONAdr = getAllOrOne(new URI(Constants.ADDRESS_SOURCE));
+	List<SurveyAnswer> surveyAnswers= mapper.readValue(JSONAdr, new TypeReference<List<SurveyAnswer>>() {
+	});
+
+	HashMap<Integer, SurveyAnswer> surveyAMap = new HashMap<Integer, SurveyAnswer>();
+
+	for (SurveyAnswer surveyAnswer : surveyAnswers) {
+		surveyAMap.put(surveyAnswer.getAnswerID(),surveyAnswer);
+	}
+	return surveyAMap;
+}
+
+}
---
+++ src/main/java/dataAccess/SurveyQuestionAcces.java
@@ -0,0 +1,35 @@
+package dataAccess;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import models.SurveyQuestion;
+
+public class SurveyQuestionAcces extends RestRequest {
+	
+	public static SurveyQuestion get(Integer surveyQuestionID) throws IOException, URISyntaxException {
+		String JSONAdr = getAllOrOne(new URI(Constants.ADDRESS_SOURCE + surveyQuestionID));
+		SurveyQuestion surveyQuestion= mapper.readValue(JSONAdr, SurveyQuestion.class);
+		return surveyQuestion;
+	}
+
+
+public static HashMap<Integer, SurveyQuestion> getAll() throws IOException, URISyntaxException {
+	String JSONAdr = getAllOrOne(new URI(Constants.ADDRESS_SOURCE));
+	List<SurveyQuestion> surveyQuestions= mapper.readValue(JSONAdr, new TypeReference<List<SurveyQuestion>>() {
+	});
+
+	HashMap<Integer, SurveyQuestion> surveyQMap = new HashMap<Integer, SurveyQuestion>();
+
+	for (SurveyQuestion surveyQuestion : surveyQuestions) {
+		surveyQMap.put(surveyQuestion.getQuestionID(), surveyQuestion);
+	}
+	return surveyQMap;
+}
+
+}
