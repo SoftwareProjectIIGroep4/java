999a33364f561f2156776907cd16fd546f1ef726
stefanosss <stefanos.stoikos@student.ehb.be>
2017-11-16 17:27:27 +0100

    Equals & hashcode toegevoegd klasse Book

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -116,6 +116,54 @@ public class Book {
 			}
 		}
 		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Book other = (Book) obj;
+			if (author == null) {
+				if (other.author != null)
+					return false;
+			} else if (!author.equals(other.author))
+				return false;
+			if (isbn != other.isbn)
+				return false;
+			if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+				return false;
+			if (publisher == null) {
+				if (other.publisher != null)
+					return false;
+			} else if (!publisher.equals(other.publisher))
+				return false;
+			if (title == null) {
+				if (other.title != null)
+					return false;
+			} else if (!title.equals(other.title))
+				return false;
+			return true;
+		}
+		
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((author == null) ? 0 : author.hashCode());
+			result = prime * result + (int) (isbn ^ (isbn >>> 32));
+			long temp;
+			temp = Double.doubleToLongBits(price);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			result = prime * result + ((publisher == null) ? 0 : publisher.hashCode());
+			result = prime * result + ((title == null) ? 0 : title.hashCode());
+			return result;
+		}
+
+		
+		
 			
 }
 	
