998b5ff950c81f6b2cd40f7cae2ae261e4ee2d0a
Jan <jan.dierickx@student.ehb.be>
2017-12-11 09:48:54 +0100

    aanmaak upload/download foto certificate, testen met GUI

--- src/main/java/dataAccess/Constants.java
+++ src/main/java/dataAccess/Constants.java
@@ -1,8 +1,8 @@
 package dataAccess;
 
 public class Constants {
-	// public static final String DATA_SOURCE = "http://10.3.50.22/api/";
-	public static final String DATA_SOURCE = "http://10.3.210.136/api/";
+	 public static final String DATA_SOURCE = "http://10.3.50.22/api/";
+	//public static final String DATA_SOURCE = "http://10.3.210.136/api/";
 	public static final String ADDRESS_SOURCE = DATA_SOURCE + "addresses/";
 	public static final String BOOK_SOURCE = DATA_SOURCE + "books/";
 	public static final String CERTIFICATE_SOURCE = DATA_SOURCE + "certificates/";
